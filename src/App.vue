e7d1083f193a741a8d5da9a740c720177ecbd6d4
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-10-27 13:22:08 +0900

    接続でけた

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>ユーザ登録画面</h2>
+    <button @click="logout">ログアウト</button>
+
+    <div>
+      <p>メールアドレス:</p>
+      <input type="text" id="email" v-model="mail" />
+      <p>パスワード:</p>
+      <input type="text" id="pass" v-model="password" />
+    </div>
+    <button type="submit" @click="register">登録する</button>
+
+    <h2>ログイン画面</h2>
+    <div class="form-group">
+      <p>メールアドレス:</p>
+      <input type="text" v-model="loginEmail" />
+      <p>パスワード:</p>
+      <input type="text" v-model="loginPassword" />
+    </div>
+    <button @click="login">ログインする</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from "./main";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      mail: "",
+      password: "",
+      loginEmail: "",
+      loginPassword: "",
+    };
+  },
+  methods: {
+    register() {
+      console.log("出力1");
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.mail, this.password)
+        .then(() => {
+          alert("メールアドレス:" + this.mail + "パスワード:" + this.password);
+          alert("登録完了");
+          console.log("出力2");
+        });
+      console.log("出力4");
+    },
+    login() {
+      console.log("出力5");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.loginEmail, this.loginPassword)
+        .then(() => {
+          alert(
+            "メールアドレス:" +
+              this.loginEmail +
+              "パスワード:" +
+              this.loginPassword
+          );
+        });
+    },
+    logout() {
+      firebase.auth().signOut();
+      alert("ログアウト完了");
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "@/App.vue";
+import firebase from "firebase";
+// import * as firebase from "firebase/app";
+// import firebase from "firebase/app";
+import "firebase/auth";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6CcyA169P5ZLaDJ0cw71h3H_XIZJeYhE",
+  authDomain: "chat-app-950d0.firebaseapp.com",
+  databaseURL: "https://chat-app-950d0-default-rtdb.firebaseio.com",
+  projectId: "chat-app-950d0",
+  storageBucket: "chat-app-950d0.appspot.com",
+  messagingSenderId: "164752078871",
+  appId: "1:164752078871:web:4d5c2c0c1f60bfd4b34452",
+  measurementId: "G-3Z71Z7F3XT",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
