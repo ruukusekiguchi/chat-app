1db5c98f0e56347076e56ae151710186e147b3a9
ruukusekiguchi <ruukure0827.1027@docomo.ne.jp>
2022-11-24 02:08:18 +0900

    a

--- src/chatPage.vue
+++ src/chatPage.vue
@@ -3,9 +3,13 @@
     <div>
       <h1>チャットページ</h1>
       <p><router-link :to="{ name: 'top' }"> ログインページ </router-link></p>
-      <h2>ログイン画面</h2>
+      <!-- <h2>ログイン画面</h2> -->
+      <input type="text" v-model="message" />
       <button @click="login">ログインする</button>
     </div>
+
+    <table hover :items="items"></table>
+    <!-- <p>{{ this.items }}</p> -->
   </div>
 </template>
 
@@ -16,8 +20,10 @@ export default {
   name: "App",
   data() {
     return {
+      message: "",
       loginEmail: "",
       loginPassword: "",
+      items: [],
     };
   },
   methods: {
@@ -30,19 +36,30 @@ export default {
         .then((response) => {
           const list = {};
 
+          this.date = new Date();
+          // this.date.getFullYear();
+          // this.date.getMonth();
+          // this.date.getDate();
+
           response.forEach((doc) => {
             const docId = doc.id;
             list[docId] = doc.data();
+            this.items = [doc.data()];
           });
+          // console.log(list[docId]);
           console.log(list);
         });
+
+      // var message ={
+      //   item:this.list[0]
+      // };
 
       //db登録処理
       firebase
         .firestore()
         .collection("colection")
         .doc()
-        .set({ text: "bbbb", timestamp: "" });
+        .set({ text: this.message, timestamp: "" });
     },
     logout() {
       firebase.auth().signOut();
